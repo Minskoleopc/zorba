9fa0360c2a0edc94c31ec9be62dc2dd07762d6e2
Minskoleopc <minskoleopc@gmail.com>
2023-01-10 08:14:54 +0530

    added

---
+++ src/one/day1.java
@@ -0,0 +1,16 @@
+package one;
+
+public class day1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("hello");
+		
+		// Installation
+		
+		
+	
+	}
+
+}
