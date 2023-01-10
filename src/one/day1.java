a2953e72b24a77fb46a2c77ff8a780e45e5cab92
Minskoleopc <minskoleopc@gmail.com>
2023-01-10 08:46:48 +0530

    added One

--- src/one/day1.java
+++ src/one/day1.java
@@ -8,9 +8,88 @@ public class day1 {
 		System.out.println("hello");
 		
 		// Installation
+		// variables??
+		// Type
+		//Extrovert -- speaks more , more social , more outing
+		//Introvert -- calm , speaks less , less outing
 		
-		
+		// Types
 	
+		// Human
+		// Property - age , weight height color
+		// Methods - talk() , walk()
+		
+		// Vehicle 
+		// Property -color type brand model number 
+		// Method - start() , stop()
+		
+		// Bank type
+		// Property - accName , accNumber , balance , IFSC code 
+		// Method - Deposit() , withdrawl()
+		
+		int x = 10; // 700 , -89 , 0
+		
+		
+		// Define a variable
+		
+		int s = 10;
+		int t = 5;
+		
+		// Arithmetic operation
+		
+		// + , - , * , / ,%
+		// + -> addition 
+		// - -> subtraction
+		// * -> multiplication
+		// / -> division 
+		// % -> modulus
+		
+		
+		System.out.println(s+t);
+		System.out.println(s-t);
+		System.out.println(s*t);
+		System.out.println(s/t);
+		System.out.println(s%t);
+		
+		
+		int a = 90;
+		int b = 45;
+		
+		System.out.println(a+b);
+		System.out.println(a-b);
+		System.out.println(a*b);
+		System.out.println(a/b);
+		System.out.println(a%b);
+		
+		
+		// 10 pairs ?? 50 lines
+		
+		Calculator(12,6);
+		Calculator(10,3);
 	}
+	
+	
+	static void Calculator(int x, int y) {
+		
+		System.out.println(x+y);
+		System.out.println(x-y);
+		System.out.println(x*y);
+		System.out.println(x/y);
+		System.out.println(x%y);
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 
 }
