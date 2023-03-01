0c6b58196094e766f5dc4d2099ea471f2f27f969
Minskoleopc <minskoleopc@gmail.com>
2023-03-01 07:52:19 +0530

    added

---
+++ src/two/day17.java
@@ -0,0 +1,133 @@
+package two;
+
+public class day17 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// Inheritance
+		// Polymorphism
+		// Abstraction
+		// Hiding 
+
+		//new WorldBankQ();
+		SBIa  y = new SBIa();
+		y.BranchName();
+		y.Loan();
+		y.Save();
+		y.displayCountryName();
+		
+		Student aa = new Student("chinmay","deshpande",23);
+	
+		aa.displayName();
+		aa.displayCity();
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+
+}
+
+abstract class WorldBankQ{
+	
+	// abstract class can have method with definition only
+	// abstract class can have normal methods
+	// abstract method
+	abstract public void Loan();
+	abstract public void Save();
+	// regular method
+	public void displayCountryName() {
+		System.out.println("India");
+	}
+	// You cannot create object of abstract class
+}
+
+
+class SBIa extends WorldBankQ{
+
+	public void Loan() {
+		// TODO Auto-generated method stub
+		
+		System.out.println("SBI loan method");
+		
+	}
+
+	public void Save() {
+		// TODO Auto-generated method stub
+		System.out.println("SBI save method");
+	}
+	
+	public void BranchName() {
+		System.out.println("SBI branch");
+	}
+	
+	
+}
+
+
+abstract  class Information{
+	
+	String firstName;
+	String lastName;
+	public Information(String firstName , String lastName) {
+		this.firstName = firstName;
+		this.lastName = lastName;		
+	}
+	
+	abstract public void displayName();
+	
+	
+}
+
+class Student extends Information {
+	
+	int rollNo;
+
+	public Student(String firstName, String lastName,int rollNo) {
+		super(firstName, lastName);
+		this.rollNo = rollNo;
+	}
+
+
+	public void displayName() {
+		// TODO Auto-generated method stub
+		System.out.println(this.firstName+ this.lastName);
+	}
+	
+	public void displayCity() {
+		System.out.println("Pune");
+	}
+	
+}
+
+
+// KFC 
+
+// Menu items 
+// chicken tikka 
+// chicken burger
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
